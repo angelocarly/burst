92f82bb029d8990ee6b5f95de8bacbac0a6db7b4
Angelo Carly <angelo.carly@protonmail.com>
2023-11-16 21:48:18 +0100

    Make more library friendly

---
+++ Burst.cmake
@@ -0,0 +1,5 @@
+set( BURST_RESOURCES_DIR ${CMAKE_CURRENT_LIST_DIR}/resources )
+set( BURST_LIB_DIR "${CMAKE_CURRENT_LIST_DIR}/lib" )
+
+include_directories( "${CMAKE_CURRENT_LIST_DIR}/lib/VulkanMemoryAllocator-Hpp/include")
+include_directories( "${CMAKE_CURRENT_LIST_DIR}/lib/VulkanMemoryAllocator/include")
